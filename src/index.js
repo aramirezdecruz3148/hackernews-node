cfe61b2f4210ba3d90f700d6daf8682fdf22595a
aramirezdecruz3148 <leigh3148@gmail.com>
2019-12-05 11:42:56 -0800

    attached prisma to context

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('./generated/prisma-client');
 
 const resolvers = {
   Query: {
@@ -26,5 +27,6 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: { prisma },
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
